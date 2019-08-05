af4dd6dd987b07d11389435268b6246c32a63806
Angela Choi <eunsol.angela.choi@gmail.com>
2019-08-05 13:15:09 -0700

    testing

--- db/config.js
+++ db/config.js
@@ -5,5 +5,8 @@ module.exports = {
       endpoint: 'http://localhost:8000'
     },
     aws_remote_config: {
+        accessKeyId: 'AKIAJDNKL2SYNZUPRNLQ',
+        secretAccessKey: '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',
+        region: 'us-east-1'
     }
   };
