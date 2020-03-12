4eed6410109b6ebb96559a650c3f5908507ca554
Angela Choi <eunsol.angela.choi@gmail.com>
2020-03-12 13:54:52 -0700

    .

--- db/config.js
+++ db/config.js
@@ -1,11 +1,1 @@
-module.exports = {
-    aws_table_name: 'game',
-    aws_local_config: {
-      region: 'local',
-      endpoint: 'http://localhost:8000'
-    },
-    aws_remote_config: {
-
-        region: 'us-east-1'
-    }
-  };
+module.exports = ''
