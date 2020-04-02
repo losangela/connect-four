65408dbbeca928efa915be2b3e1cf5ce28e64c81
Angela Choi <eunsol.angela.choi@gmail.com>
2020-04-01 21:29:52 -0700

    lobby css done

--- client/src/components/RoomView.jsx
+++ client/src/components/RoomView.jsx
@@ -2,13 +2,26 @@ import React, { useState, useEffect } from 'react';
 
 const RoomView = ({room, switchRoom}) => {
   
+  const status = (b) => {
+    if (b) {
+      return (
+        <div classId="game-status">
+          GAME PLAYING
+        </div>
+      )
+    } else {
+      return (
+        <div className="game-status">
+          WAITING TO PLAY
+        </div>
+      )
+    }
+  }
   return (
     <div className="room-view-container">
-      <h3>Room # {room.id}</h3>
-      {room.players.length} players in room. <br />
-      Red: {room.playerRed ? room.playerRed.name : "none"}<br/>
-      Yellow: {room.playerYellow? room.playerYellow.name : "none"}<br/>
-      {room.isPlaying ? "Game Started!" : "Waiting to start"} <br/>
+      <div className="room-view-label">Room # {room.id}</div>
+      <div className="room-view-player">{room.players.length} players in room. </div>
+      {status(room.isPlaying)} <br/>
       <button id="join" onClick={() => switchRoom(room.id)}>Join room</button>
     </div>
   )
